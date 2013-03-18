750805c9fcc45d45d3ae1c5e96c83099f08cc0c1
Jussi Pakkanen <jussi.pakkanen@canonical.com>
2013-03-18 14:59:39 +0200

    Can display string list. Hoo ray.

--- tools/viewer.cpp
+++ tools/viewer.cpp
@@ -1,6 +1,16 @@
 #include <QApplication>
+#include <QListView>
+#include <QStringListModel>
 
 int main(int argc, char **argv) {
     QApplication app(argc, argv);
-    return 0;
+    QListView *view = new QListView(0);
+    QStringList list;
+    list << "one" << "two" << "three";
+    QStringListModel *m = new QStringListModel();
+    m->setStringList(list);
+    view->setModel(m);
+    view->show();
+    view->setWindowTitle("List viewer test app");
+    return app.exec();
 }
