2bf21d347c26ea9a37bc8d7ea38f163ef69ac751
Jussi Pakkanen <jussi.pakkanen@canonical.com>
2013-03-25 14:50:11 +0200

    Can create a Qml view.

---
+++ tools/datatest.qml
@@ -0,0 +1,7 @@
+import QtQuick 2.0
+
+Text {
+    width: 100; height: 100
+    text: "oo"
+
+}
---
+++ tools/qmltest.cpp
@@ -0,0 +1,19 @@
+#include "notification.h"
+
+#include <QGuiApplication>
+#include <QQmlEngine>
+#include <QQmlContext>
+#include <QQmlComponent>
+#include <QQuickView>
+
+int main(int argc, char *argv[]) {
+    QGuiApplication app(argc, argv);
+    QQuickView view;
+
+    //Notification msg;
+    //view.rootContext()->setContextProperty("msg", &msg);
+    /* Hardcoded URLs are bad but tolerable here in test code. */
+    view.setSource(QUrl::fromLocalFile("../tools/datatest.qml"));
+    view.show();
+    return app.exec();
+}
