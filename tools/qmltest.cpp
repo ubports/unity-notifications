0d3ca1fe5d957a4cb480b76ed65ddcf82de9b67d
Jussi Pakkanen <jussi.pakkanen@canonical.com>
2013-03-25 15:00:09 +0200

    Make Notification owned by a shared pointer.

--- tools/qmltest.cpp
+++ tools/qmltest.cpp
@@ -10,8 +10,9 @@ int main(int argc, char *argv[]) {
     QGuiApplication app(argc, argv);
     QQuickView view;
 
-    Notification msg;
-    view.rootContext()->setContextProperty("msg", &msg);
+    QSharedPointer<Notification> msg(new Notification());
+    // Problem: http://qt-project.org/wiki/SharedPointersAndQmlOwnership
+    view.rootContext()->setContextProperty("msg", msg.data());
     /* Hardcoded URLs are bad but tolerable here in test code. */
     view.setSource(QUrl::fromLocalFile("../tools/datatest.qml"));
     view.show();
