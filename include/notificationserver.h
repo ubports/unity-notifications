070ae67423627874d285b0a4593e5c6c05ebb7ac
Jussi Pakkanen <jussi.pakkanen@canonical.com>
2013-03-27 18:10:02 +0200

    Started work on DBus service.

---
+++ include/notificationserver.h
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2013 Canonical, Ltd.
+ *
+ * Authors:
+ *    Jussi Pakkanen <jussi.pakkanen@canonical.com>
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as published
+ * by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef NOTIFICATIONSERVER_H
+#define NOTIFICATIONSERVER_H
+
+#include"notify-backend.h"
+
+/* http://www.galago-project.org/specs/notification/0.9/x408.html#commands
+ *
+ * The functions to implement:
+ *
+ * STRING_ARRAY org.freedesktop.Notifications.GetCapabilities (void);
+ * UINT32 org.freedesktop.Notifications.Notify (STRING app_name, UINT32 replaces_id,
+ *   STRING app_icon, STRING summary, STRING body, ARRAY actions, DICT hints, INT32 expire_timeout);
+ * void org.freedesktop.Notifications.CloseNotification (UINT32 id);
+ * void org.freedesktop.Notifications.GetServerInformation (out STRING name, out STRING vendor, out STRING version);
+ *
+ * Signals:
+ * org.freedesktop.Notifications.NotificationClosed (UINT32 id, UINT32 reason);
+ * org.freedesktop.Notifications.ActionInvoked (UINT32 id, STRING action_key);
+ *
+ */
+
+#include <QDBusAbstractAdaptor>
+
+#define DBUS_SERVICE_NAME "com.canonical.notificationproto" // org.freedesktop.Notifications
+#define DBUS_INTERFACE "org.freedesktop.Notifications"
+#define DBUS_PATH "/org/freedesktop.Notifications"
+
+class NotificationServer : public QDBusAbstractAdaptor {
+    Q_OBJECT
+    Q_CLASSINFO("D-Bus Interface", DBUS_SERVICE_NAME)
+
+public:
+    NotificationServer(QObject *parent);
+    ~NotificationServer();
+
+public slots:
+    QList<QString> GetCapabilities() const;
+};
+
+#endif
---
+++ src/notificationserver.cpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2013 Canonical, Ltd.
+ *
+ * Authors:
+ *    Jussi Pakkanen <jussi.pakkanen@canonical.com>
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as published
+ * by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "notificationserver.h"
+
+
+NotificationServer::NotificationServer(QObject *parent) : QDBusAbstractAdaptor(parent) {
+
+}
+
+NotificationServer::~NotificationServer() {
+
+}
+
+QList<QString> NotificationServer::GetCapabilities() const {
+    QList<QString> capabilities;
+    capabilities.push_back("cap1");
+    capabilities.push_back("cap2");
+    return capabilities;
+}
