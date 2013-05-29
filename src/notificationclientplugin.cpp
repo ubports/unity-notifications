581ef2ad2134507615abbd3b29704e6b5f22c703
Jussi Pakkanen <jussi.pakkanen@canonical.com>
2013-05-29 11:21:09 +0300

    Made notification client a proper plugin.

---
+++ src/notificationclientplugin.cpp
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2013 Canonical, Ltd.
+ *
+ * Authors:
+ *    Jussi Pakkanen <jussi.pakkanen@canonical.com>
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as published
+ * by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "notificationclient.h"
+#include "notificationclientplugin.h"
+
+#include <qqml.h>
+
+#include <QtDBus>
+#include <QQmlEngine>
+#include <QQmlContext>
+
+void NotificationClientPlugin::registerTypes(const char *uri) {
+    qmlRegisterUncreatableType<NotificationClient>(uri, 1, 0, "NotificationClient", "");
+}
+
+void NotificationClientPlugin::initializeEngine(QQmlEngine *engine, const char *uri) {
+    NotificationClient *cl = new NotificationClient(engine);
+    QDBusInterface service(DBUS_SERVICE_NAME, DBUS_PATH, DBUS_INTERFACE);
+    if(!QDBusConnection::sessionBus().connect(DBUS_SERVICE_NAME, DBUS_PATH, DBUS_INTERFACE,
+            "NotificationClosed", cl, SLOT(NotificationClosed(unsigned int, unsigned int)))) {
+        printf("Could not connect to NotificationClosed signal.\n\n");
+    }
+    if(!QDBusConnection::sessionBus().connect(DBUS_SERVICE_NAME, DBUS_PATH, DBUS_INTERFACE,
+            "ActionInvoked", cl, SLOT(ActionInvoked(unsigned int, QString)))) {
+        printf("Could not connect to ActionInvoked signal.\n\n");
+    }
+    engine->rootContext()->setContextProperty("notificationclient", cl);
+}
