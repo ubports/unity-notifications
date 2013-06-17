9cf343641ed1d41119ca35e904b2cb9fc9d1c1b0
Jussi Pakkanen <jussi.pakkanen@canonical.com>
2013-06-17 16:23:26 +0300

    Reformatted constructor with lots of parameters for clarity.

--- include/notification.h
+++ include/notification.h
@@ -78,8 +78,18 @@ public Q_SLOTS:
 
 public:
     Notification(QObject *parent=nullptr);
-    Notification(NotificationID id, int displayTime, const Urgency ur, QString text, Type type=Ephemeral, NotificationServer *srv=nullptr, QObject *parent=nullptr);
-    Notification(NotificationID id, int displayTime, const Urgency ur, Type type=Ephemeral, NotificationServer *srv=nullptr, QObject *parent=nullptr);
+    Notification(NotificationID id,
+            int displayTime,
+            const Urgency ur,
+            QString text, Type type=Ephemeral,
+            NotificationServer *srv=nullptr,
+            QObject *parent=nullptr);
+    Notification(NotificationID id,
+            int displayTime,
+            const Urgency ur,
+            Type type=Ephemeral,
+            NotificationServer *srv=nullptr,
+            QObject *parent=nullptr);
     virtual ~Notification();
 
     NotificationID getID() const;
