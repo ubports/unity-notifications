129fa7fed669907c8545a0d19ca43be5693317ad
Mirco Müller <mirco.mueller@ubuntu.com>
2013-09-11 15:11:27 +0200

    Made the stand-alone Python-example triggering the simunlock-dialog work correctly now.

--- examples/sd-example-simunlock.py
+++ examples/sd-example-simunlock.py
@@ -40,6 +40,13 @@ SIM_UNLOCK_MENU_PATH = '/com/canonical/indicator/network/unlocksim'
 SIM_UNLOCK_ACTION_PATH = '/com/canonical/indicator/network/unlocksim'
 
 def quit_callback(notification, loop):
+	global connection
+	global exported_action_group_id
+	global exported_menu_model_id
+
+	connection.unexport_action_group (exported_action_group_id)
+	connection.unexport_menu_model (exported_menu_model_id)
+
 	loop.quit()
 
 def cancel (notification, action, data):
@@ -50,13 +57,14 @@ def cancel (notification, action, data):
 
 def accept (notification, action, data):
 	if action == "accept":
-		print "Accept"
+		global pin
+		print "PIN entered: " + pin
 	else:
 		print "That should not have happened (accept)!"
 
 def simpin_changed (action, variant):
-	print "SIM Pin Changed"
-	print "pin: " + variant.get_string();
+	global pin
+	pin = variant.get_string();
 
 def bus_acquired(bus, name):
 	# menu
@@ -71,8 +79,14 @@ def bus_acquired(bus, name):
 	action.connect ("change-state", simpin_changed);
 	unlock_actions.insert (action);
 
-	bus.export_action_group(SIM_UNLOCK_ACTION_PATH, unlock_actions)
-	bus.export_menu_model(SIM_UNLOCK_MENU_PATH, unlock_menu)
+	global connection
+	connection = bus
+
+	global exported_action_group_id
+	exported_action_group_id = connection.export_action_group(SIM_UNLOCK_ACTION_PATH, unlock_actions)
+
+	global exported_menu_model_id
+	exported_menu_model_id = connection.export_menu_model(SIM_UNLOCK_MENU_PATH, unlock_menu)
 
 def pushNotification (title, body, icon):
 	n = Notify.Notification.new(title, body, icon);
@@ -88,12 +102,26 @@ def pushNotification (title, body, icon):
 			GLib.Variant.new_variant(GLib.Variant.new_string(SIM_UNLOCK_ACTION_PATH)));
 	menu_model_actions.add_value (entry);
 
+	menu_model_paths = GLib.VariantBuilder.new (GLib.VariantType.new ("a{sv}"));
+	entry = GLib.Variant.new_dict_entry(GLib.Variant.new_string("busName"),
+			GLib.Variant.new_variant(GLib.Variant.new_string(APPLICATION_ID)));
+	menu_model_paths.add_value (entry);
+	entry = GLib.Variant.new_dict_entry(GLib.Variant.new_string("menuPath"),
+			GLib.Variant.new_variant(GLib.Variant.new_string(SIM_UNLOCK_MENU_PATH)));
+	menu_model_paths.add_value (entry);
+	entry = GLib.Variant.new_dict_entry(GLib.Variant.new_string("actions"),
+			GLib.Variant.new_variant(menu_model_actions.end()));
+	menu_model_paths.add_value (entry);
+
+	n.set_hint ("x-canonical-private-menu-model", menu_model_paths.end ());
+
 	# indicate to the notification-daemon, that we want to use snap-decisions
 	n.set_hint_string ("x-canonical-snap-decisions", "true");
+	n.set_hint_string ("x-canonical-ext-snap-decisions", "true");
 	n.set_hint_string ("x-canonical-private-button-tint", "true");
-	n.set_hint_string ("x-canonical-dbus-name", APPLICATION_ID);
-	n.set_hint ("x-canonical-actions-path", menu_model_actions.end());
-	n.set_hint_string ("x-canonical-menu-path", SIM_UNLOCK_MENU_PATH);
+	#n.set_hint_string ("x-canonical-dbus-name", APPLICATION_ID);
+	#n.set_hint ("x-canonical-actions-path", menu_model_actions.end());
+	#n.set_hint_string ("x-canonical-menu-path", SIM_UNLOCK_MENU_PATH);
 
 	Gio.bus_own_name(Gio.BusType.SESSION, APPLICATION_ID, 0, bus_acquired, None, None)
 
@@ -102,6 +130,11 @@ def pushNotification (title, body, icon):
 if __name__ == '__main__':
 	if not Notify.init("sd-example-simunlock"):
 		sys.exit (1)
+
+	connection = None
+	exported_menu_model_id = 0
+	exported_action_group_id = 0
+	pin = "0000"
 
 	loop = GLib.MainLoop()
 	n = pushNotification ("Unlock SIM-card", "", "")
