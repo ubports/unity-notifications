4f99708b70b3866a2fe1ce6a3fce1912df9d406d
Mirco Müller <mirco.mueller@ubuntu.com>
2013-09-11 12:11:43 +0200

    Furhter attempt to get the stand-alone simunlock-triggering example working.

--- examples/sd-example-simunlock.py
+++ examples/sd-example-simunlock.py
@@ -42,25 +42,45 @@ SIM_UNLOCK_ACTION_PATH = '/com/canonical/indicator/network/unlocksim'
 def quit_callback(notification, loop):
 	loop.quit()
 
-def action_decline_1 (notification, action, data):
-	if action == "action_decline_1":
-		print "Decline"
+def cancel (notification, action, data):
+	if action == "cancel":
+		print "Cancel"
 	else:
-		print "That should not have happened (action_decline_1)!"
+		print "That should not have happened (cancel)!"
 
-def action_accept (notification, action, data):
-	if action == "action_accept":
-		print "Accepting call"
+def accept (notification, action, data):
+	if action == "accept":
+		print "Accept"
 	else:
-		print "That should not have happened (action_accept)!"
+		print "That should not have happened (accept)!"
+
+def simpin_changed (action, variant):
+	print "SIM Pin Changed"
+	print "pin: " + variant.get_string();
+
+def bus_acquired(bus, name):
+	# menu
+	unlock_menu = Gio.Menu();
+	pin_unlock = Gio.MenuItem.new ("", "notifications.simunlock");
+	pin_unlock.set_attribute_value ("x-canonical-type", GLib.Variant.new_string("com.canonical.snapdecision.pinlock"));
+	unlock_menu.append_item (pin_unlock);
+
+	# actions
+	unlock_actions = Gio.SimpleActionGroup.new();
+	action = Gio.SimpleAction.new_stateful("simunlock", GLib.VariantType.new("s"), GLib.Variant.new_string(""));
+	action.connect ("change-state", simpin_changed);
+	unlock_actions.insert (action);
+
+	bus.export_action_group(SIM_UNLOCK_ACTION_PATH, unlock_actions)
+	bus.export_menu_model(SIM_UNLOCK_MENU_PATH, unlock_menu)
 
 def pushNotification (title, body, icon):
 	n = Notify.Notification.new(title, body, icon);
 
 	# NOTE: the order in which actions are added is important... positive
 	# always comes first!
-	n.add_action ("action_accept",    "Accept", action_accept, None);
-	n.add_action ("action_decline_1", "Decline", action_decline_1, None);
+	n.add_action ("accept", "Accept", accept, None);
+	n.add_action ("cancel", "Cancel", cancel, None);
 
 	# create the menu-model
 	menu_model_actions = GLib.VariantBuilder.new (GLib.VariantType.new ("a{sv}")); 
@@ -74,6 +94,8 @@ def pushNotification (title, body, icon):
 	n.set_hint_string ("x-canonical-dbus-name", APPLICATION_ID);
 	n.set_hint ("x-canonical-actions-path", menu_model_actions.end());
 	n.set_hint_string ("x-canonical-menu-path", SIM_UNLOCK_MENU_PATH);
+
+	Gio.bus_own_name(Gio.BusType.SESSION, APPLICATION_ID, 0, bus_acquired, None, None)
 
 	return n
 
